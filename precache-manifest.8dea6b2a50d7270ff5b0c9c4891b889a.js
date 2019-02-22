09b6ef7b489ea1caa666013b581b92a776144c2d
ashleyxwang <ashleywang514@yahoo.com>
2019-02-22 17:53:11 -0600

    Updates

---
+++ precache-manifest.8dea6b2a50d7270ff5b0c9c4891b889a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61a0df336c60dbe48a52",
+    "url": "/Clicky-Game/static/js/runtime~main.61a0df33.js"
+  },
+  {
+    "revision": "36db40bffba02c10cfd2",
+    "url": "/Clicky-Game/static/js/main.36db40bf.chunk.js"
+  },
+  {
+    "revision": "a080abdb6f85b3109260",
+    "url": "/Clicky-Game/static/js/2.a080abdb.chunk.js"
+  },
+  {
+    "revision": "36db40bffba02c10cfd2",
+    "url": "/Clicky-Game/static/css/main.91847b83.chunk.css"
+  },
+  {
+    "revision": "d817ebbfb65d6777fbdb178a0ddea2a2",
+    "url": "/Clicky-Game/index.html"
+  }
+];
